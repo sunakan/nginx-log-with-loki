b4e91e146c83a30b2b38f0cd3cfc9429e02f68ac
sunakan <sunakan03@gmail.com>
2024-10-19 21:18:38 +0900

    k6: curlを何度もする手間を省くため、k6を利用

---
+++ k6/scripts/simple.js
@@ -0,0 +1,16 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+  // VU数: 仮想ユーザー
+  stages: [
+    { duration: "15s", target: 15 }, // 15秒かけてでVU数を0から15まで徐々に増加
+    { duration: "30s", target: 15 }, // 10秒間、VU数を15で維持
+    { duration: "15s", target: 0  }, // 15秒かけて、VU数を15から0まで徐々に減少
+  ],
+};
+
+export default function () {
+  let res = http.get("http://nginx:80/");
+  check(res, { "status was 200": (r) => r.status == 200 });
+}
